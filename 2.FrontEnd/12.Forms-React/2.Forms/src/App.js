42dcc0924517c278314d4359424deda72b6c7254
Carol Bezerra <carol.arbe@gmail.com>
2020-12-17 10:55:48 -0300

    add day 12.2

---
+++ 2.FrontEnd/12.Forms-React/2.Forms/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Forms from './Forms';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Forms />
+    )
+  }
+};
+
+
+export default App;
